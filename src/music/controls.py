162a288f43444cb8c5b22b54afd3738d8b51c25b
Roaham <roripro2007@gmail.com>
2025-12-02 22:25:01 +0100

    Add files via upload

--- src/music/controls.py
+++ src/music/controls.py
@@ -2,11 +2,13 @@ import discord
 from discord.ui import View, Button
 
 class MusicControls(View):
-    def __init__(self, vc, queue, next_callback):
+    def __init__(self, vc, queue, next_callback, loop_enabled, guild_id):
         super().__init__(timeout=None)
         self.vc = vc
         self.queue = queue
         self.next_callback = next_callback
+        self.loop_enabled = loop_enabled
+        self.guild_id = guild_id
 
     @discord.ui.button(label="Play/Pause", style=discord.ButtonStyle.primary)
     async def play_pause(self, interaction: discord.Interaction, button: Button):
@@ -37,3 +39,15 @@ class MusicControls(View):
             await interaction.response.send_message(ephemeral=True)
         else:
             await interaction.response.send_message("No hay música en reproducción.", ephemeral=True)
+
+    @discord.ui.button(label="Loop", style=discord.ButtonStyle.success)
+    async def loop(self, interaction: discord.Interaction, button: Button):
+
+        current = self.loop_enabled.get(self.guild_id, False)
+        self.loop_enabled[self.guild_id] = not current
+
+        estado = "Activo" if not current else "Inactivo"
+
+        await interaction.response.send_message(
+            f"Loop {estado}", ephemeral=True
+        )
